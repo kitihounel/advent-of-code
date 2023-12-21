e6f6449a34766bf2c2893ec798be8d67ea92a148
kitihounel <kitihounel@gmail.com>
2023-12-21 21:20:28 +0100

    challenges: day 20 part 1

---
+++ 2023/day-20/a.py
@@ -0,0 +1,109 @@
+from sys import stdin
+from collections import deque, namedtuple
+from itertools import chain
+
+
+Pulse = namedtuple('Pulse', ['src', 'dst', 'type'])
+queue = deque()
+
+
+class Module:
+    book = {}
+
+    def __init__(self, name, t, output_module_names) -> None:
+        self.name = name
+        self.type = t
+        self.output_module_names = output_module_names
+        self.state = False
+        self.memory = {}
+        type(self).book[name] = self
+
+
+    def add_input(self, name):
+        self.memory[name] = 'low'
+
+
+    @classmethod
+    def connect_modules(cls):
+        for m in cls.book.values():
+            for name in m.output_module_names:
+                other = cls.book.get(name)
+                other.add_input(m.name)
+
+
+    @classmethod
+    def get(cls, name):
+        return cls.book.get(name, None)
+
+
+    def process(self, pulse):
+        if self.type == 'c':
+            self._process_as_conjunction(pulse)
+        elif self.type == 'f':
+            self._process_as_flip_flop(pulse)
+        elif self.type == 'b':
+            self._process_as_broadcaster(pulse)
+        else:
+            pass
+
+
+    def _process_as_broadcaster(self, pulse):
+        for name in self.output_module_names:
+            queue.append(Pulse(self.name, name, pulse.type))
+
+
+    def _process_as_flip_flop(self, pulse):
+        if pulse.type == 'high':
+            return
+        self.state = not self.state
+        t = 'high' if self.state else 'low'
+        for name in self.output_module_names:
+            queue.append(Pulse(self.name, name, t))
+
+
+    def _process_as_conjunction(self, pulse):
+        self.memory[pulse.src] = pulse.type        
+        t = 'low' if all(v == 'high' for v in self.memory.values()) else 'high'
+        for name in self.output_module_names:
+            queue.append(Pulse(self.name, name, t))
+
+
+def get_module_type(s):
+    if s == 'broadcaster':
+        return 'b'
+    ch = s[0]
+    return 'f' if ch == '%' else 'c' if ch == '&' else 'o'
+
+
+def read_input():
+    modules = []
+    names = set()
+    for line in stdin:
+        chunks = [s.strip() for s in line.strip().split('->')]
+        t = get_module_type(chunks[0])
+        name = chunks[0] if t in ('b', 'o') else chunks[0][1:]
+        outputs = [s.strip() for s in chunks[1].split(',')]
+        modules.append(Module(name, t, outputs))
+        names.update(chain([name], outputs))
+    return modules, names
+
+
+modules, names = read_input()
+for name in filter(lambda s: Module.get(s) is None, names):
+    m = Module(name, 'o', [])
+    modules.append(m)
+Module.connect_modules()
+
+low_count, hi_count = 0, 0
+for _ in range(1000):
+    queue.append(Pulse(None, 'broadcaster', 'low'))
+    while len(queue) != 0:
+        pulse = queue.popleft()
+        if pulse.type == 'low':
+            low_count += 1
+        else:
+            hi_count += 1
+        m = Module.get(pulse.dst)
+        if m is not None:
+            m.process(pulse)
+print(hi_count * low_count)
